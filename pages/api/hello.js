3343f9d7f9a64525b6b0574ce2c1a948c19246f7
Masfikul Alam <masfikalamfrp@gmail.com>
2021-02-14 22:52:24 +0600

    next js blog application

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+  const data = [
+    {
+      name: 'Masfikul Alam',
+      dob: '2nd July 2003',
+      city: 'Chittagong',
+      country: 'Bangladesh',
+      email: 'masfikalamfrp@gmail.com'
+    },
+    {
+      name: 'Jhankar Mahbub',
+      dob: '15th May 2003',
+      city: 'Chicago',
+      country: 'USA',
+      email: 'jhankar.mahbub1@gmail.com'
+    }
+  ]
+  res.status(200).json(data)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,50 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import utilStyles from '../styles/utils.module.css'
+import Layout from '../components/layout'
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export default function Home({ allPostsData }) {  
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
+        <title>Next - Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h2>Summary</h2>
+      <p>I'm <u>Masfikul Alam</u> , a self-taught programmer having expertise in modern front-end technologies and a deep interest in JavaScript. I desire to work in the software industry with a good learning environment to grow my skills to the next level.</p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link> <br />
+              <small className={utilStyles.lightText}>{id} / {date}</small>
+            </li>
+          ))}
+        </ul>
+      </section>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      <h2>Portfolio</h2>
+      <p>Visit <a href="https://masfikul-alam.web.app">my website</a> to see my projects.</p>
+      
+      <h2>Contact</h2>
+      <a href="mailto:masfikalamfrp@gmail.com">Email</a>,{' '}
+      <a href="https://github.com/masfikalam">Github</a>,{' '}
+      <a href="https://linkedin.com/in/masfik-alam">Linkedin</a>
+    </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
